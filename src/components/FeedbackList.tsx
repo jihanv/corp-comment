52d6254e72907f05b1d3b4972d92bb9da4b2a80a
jihanv <hedjmny@gmail.com>
2025-07-25 20:28:55 +0900

    Make api call for data

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,20 +1,32 @@
+import { useEffect, useState } from "react"
 import FeedbackItem from "./FeedbackItem"
 
-const feedbackItems = [{
-    upvoteCount: 563,
-    badgeLetter: "S",
-    companyName: "Starbucks",
-    text: "Blah",
-    daysAgo: 3
-},
-{
-    upvoteCount: 234,
-    badgeLetter: "D",
-    companyName: "McDonalds",
-    text: "BOOO",
-    daysAgo: 3
-}]
+// const exampleFeedbackItems = [{
+//     upvoteCount: 563,
+//     badgeLetter: "S",
+//     companyName: "Starbucks",
+//     text: "Blah",
+//     daysAgo: 3
+// },
+// {
+//     upvoteCount: 234,
+//     badgeLetter: "D",
+//     companyName: "McDonalds",
+//     text: "BOOO",
+//     daysAgo: 3
+// }]
 export default function FeedbackList() {
+
+    const [feedbackItems, setFeedbackItems] = useState([])
+
+    useEffect(() => {
+        fetch("https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks")
+            .then(res => res.json())
+            .then(data => {
+                setFeedbackItems(data.feedbacks)
+            })
+    }, [])
+
     return (
         <ol className="feedback-list">
             {feedbackItems.map((feedbackItem) => {
