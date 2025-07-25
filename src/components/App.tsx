2563982f35a75cfb593d1ebebcde10cf558fda82
jihanv <hedjmny@gmail.com>
2025-07-25 15:54:58 +0900

    Add component skeletal structure

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import Container from "./Container"
+import Footer from "./Footer"
+import HashtagList from "./HashtagList"
+
+function App() {
+
+  return (
+    <div className="app">
+      <Footer />
+      <Container />
+      <HashtagList />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="footer"><small>
+            <p>Intended for learning</p>
+        </small></footer>
+    )
+}
