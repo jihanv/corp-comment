199c75e47fd30eb29a0e49b1ed017588605ba272
jihanv <hedjmny@gmail.com>
2025-07-25 16:48:39 +0900

    Changed textare from uncontrolled to controlled

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
 
 export default function FeedbackForm() {
+
+    const [text, setText] = useState("")
+
+    const charCount = MAX_CHARACTERS - text.length
     return (
         <>
             <form className="form">
-                <textarea id="feedback-texarea" placeholder="" spellCheck={false} />
-                <label htmlFor="feedback-textarea">Enter your feedback here. Remember to #hashtag the company.</label>
+                <textarea
+                    onChange={(event) => { setText(event.target.value) }}
+                    value={text}
+                    id="feedback-texarea"
+                    placeholder=""
+                    spellCheck={false} />
+                <label htmlFor="feedback-textarea">
+                    Enter your feedback here. Remember to #hashtag the company.
+                </label>
                 <div>
-                    <p className="u-italic">150</p>
-                    <button><span>Submit</span></button>
-
+                    <p className="u-italic">{charCount}</p>
+                    <button>
+                        <span>Submit</span>
+                    </button>
                 </div>
             </form>
         </>
-    )
+    );
 }
