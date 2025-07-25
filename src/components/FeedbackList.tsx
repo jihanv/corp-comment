66b16871dd8f7f0d9096f58a82f7158f4c88098b
jihanv <hedjmny@gmail.com>
2025-07-25 21:00:11 +0900

    Add error catching for data fetch

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import FeedbackItem from "./FeedbackItem"
+import Spinner from "./Spinner"
+import ErrorMessage from "./ErrorMessage"
 
 // const exampleFeedbackItems = [{
 //     upvoteCount: 563,
@@ -18,17 +20,36 @@ import FeedbackItem from "./FeedbackItem"
 export default function FeedbackList() {
 
     const [feedbackItems, setFeedbackItems] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [errorMessage, setErrorMessage] = useState("")
 
     useEffect(() => {
+        setIsLoading(true)
         fetch("https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks")
-            .then(res => res.json())
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error()
+                }
+                return res.json()
+            })
             .then(data => {
                 setFeedbackItems(data.feedbacks)
+                setIsLoading(false)
+            })
+            .catch(() => {
+                setIsLoading(false)
+                setErrorMessage("Something went wrong.")
             })
     }, [])
 
     return (
         <ol className="feedback-list">
+            {
+                isLoading ? <Spinner /> : null
+            }
+            {
+                errorMessage ? <ErrorMessage message={errorMessage} /> : null
+            }
             {feedbackItems.map((feedbackItem) => {
                 return <FeedbackItem feedbackItem={feedbackItem} />
             })}
